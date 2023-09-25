6498373df2c0aa19357913477b117ad3fd0d931b
Alash <aldarzhaj@gmail.com>
2023-09-25 11:32:17 +0700

    first

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,80 +1,83 @@
-import { Component } from "react";
+import { Component } from 'react'
 
-import NewTaskForm from "../NewTaskForm/NewTaskForm";
-import TaskList from "../TaskList";
-import Footer from "../Footer";
-import "./App.css";
+import NewTaskForm from '../NewTaskForm/NewTaskForm'
+import TaskList from '../TaskList'
+import Footer from '../Footer'
+import './App.css'
 
 export default class App extends Component {
-  maxId = 100;
+  maxId = 100
+
   state = {
     todoData: [
-      this.createTodoItem("Completed task"),
-      this.createTodoItem("Editing task"),
-      this.createTodoItem("Active task"),
+      this.createTodoItem('Completed task'),
+      this.createTodoItem('Editing task'),
+      this.createTodoItem('Active task'),
     ],
-    filter: "all",
-  };
+    filter: 'all',
+  }
 
   createTodoItem(label) {
     return {
       label,
       done: false,
       edit: false,
+      // eslint-disable-next-line no-plusplus
       id: this.maxId++,
-    };
+    }
   }
 
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
       return {
         todoData: todoData.filter((el) => el.id !== id),
-      };
-    });
-  };
+      }
+    })
+  }
 
   deletedCompletedItems = () => {
     this.setState(({ todoData }) => {
-      const newArr = todoData.filter((el) => !el.done);
+      const newArr = todoData.filter((el) => !el.done)
       return {
         todoData: newArr,
-      };
-    });
-  };
+      }
+    })
+  }
 
   addItem = (text) => {
     const newItem = {
       label: text,
       done: false,
+      // eslint-disable-next-line no-plusplus
       id: this.maxId++,
-    };
+    }
     this.setState(({ todoData }) => {
-      const newArr = [...todoData, newItem];
+      const newArr = [...todoData, newItem]
       return {
         todoData: newArr,
-      };
-    });
-  };
+      }
+    })
+  }
 
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
       return {
         todoData: todoData.map((item) => {
-          return item.id === id ? { ...item, done: !item.done } : item;
+          return item.id === id ? { ...item, done: !item.done } : item
         }),
-      };
-    });
-  };
+      }
+    })
+  }
 
   onToggleEdit = (id) => {
     this.setState(({ todoData }) => {
       return {
         todoData: todoData.map((item) => {
-          return item.id === id ? { ...item, edit: true } : item;
+          return item.id === id ? { ...item, edit: true } : item
         }),
-      };
-    });
-  };
+      }
+    })
+  }
 
   updateTask = (labelUpdate, id) => {
     this.setState(({ todoData }) => {
@@ -82,26 +85,30 @@ export default class App extends Component {
         todoData: todoData.map((item) => {
           return item.id === id
             ? { ...item, label: labelUpdate, edit: false }
-            : item;
+            : item
         }),
-      };
-    });
-  };
+      }
+    })
+  }
 
   onFilterChange = (filter) => {
-    this.setState({ filter });
-  };
+    this.setState({ filter })
+  }
 
+  /* eslint no-nested-ternary: "error" */
   render() {
-    const { todoData, filter, edit } = this.state;
-    const doneCount = todoData.filter((el) => el.done).length;
-    const todoCount = todoData.length - doneCount;
-    const filteredItems =
-      filter === "all"
-        ? todoData
-        : filter === "active"
-        ? todoData.filter((item) => !item.done)
-        : todoData.filter((item) => item.done);
+    const { todoData, filter, edit } = this.state
+    const doneCount = todoData.filter((el) => el.done).length
+    const todoCount = todoData.length - doneCount
+    let filteredItems
+
+    if (filter === 'all') {
+      filteredItems = todoData
+    } else if (filter === 'active') {
+      filteredItems = todoData.filter((item) => !item.done)
+    } else {
+      filteredItems = todoData.filter((item) => item.done)
+    }
 
     return (
       <section className="todoapp">
@@ -124,6 +131,6 @@ export default class App extends Component {
           onToggleDone={this.onToggleDone}
         />
       </section>
-    );
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client'
 
-import App from "./components/App";
-import "./index.css";
+import App from './components/App'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<App />);
+root.render(<App />)
